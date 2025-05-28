d664eeb827dca10d15f73811b44a8f56b21973f7
Eijiro1016 <wse818181@gmail.com>
2025-05-28 14:19:23 +0800

    遊戲讀取畫面完成

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
     // ~~ 1. Controls All Player Movement
     // ~~ 2. Updates Animator to Play Idle & Walking Animations
 
-    private float speed = 10f;
+    [SerializeField] private float speed = 10f;
     private Rigidbody2D myRigidbody;
     private Vector3 playerMovement;
     private Animator animator;
