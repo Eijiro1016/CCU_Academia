41165297d4b689dccbb48fa6b3cd5d7724498e39
Eijiro1016 <wse818181@gmail.com>
2025-05-29 16:21:28 +0800

    Revert "Merge pull request #2 from Eijiro1016/fix-bug"
    This reverts commit 2fdd3de1fe2f61afc1c792b046a7b3b8101a12dd, reversing
    changes made to 197d7c7952c92eaa35e5271c9ab4d41ce784f041.

---
+++ Assets/Script/EndingSceneManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingSceneManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("CharacterCreator");  // 替換成遊戲主場景名稱
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");   // 替換成主選單場景名稱
+    }
+}
